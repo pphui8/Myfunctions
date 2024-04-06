53a334e8299bc162535d6f53a87e8d9f8d913f12
pphui8 <55843852+pphui8@users.noreply.github.com>
2024-04-06 18:35:30 +0900

    Create Head.cpp

---
+++ Head.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+using namespace std;
+struct __INIT{__INIT(){cin.tie(0);ios::sync_with_stdio(false);cout<<fixed<<(15);}};
+typedef long long lint;
+typedef __int128_t llint;
+#define INF (1 << 30)
+#define LINF (lint)(1LL << 56)
+#define MINF (lint)(2e18)
+#define endl "\n"
+#define rep(i, n) for (lint(i) = 0; (i) < (n); (i)++)
+#define rep_1(i, n) for (lint(i) = 1; (i) <= (n); (i)++)
+#define yesret() cout<<"Yes"<< endl; return 0;
+#define noret() cout<<"No"<< endl; return 0;
+#define all(x) x.begin(), x.end()
+#define vec vector<lint>
+const int MAX_N = 3e5 + 5;
+// constexpr int MOD=1000000007;
+// constexpr int MOD=998244353;
+
+int main() {
+
+    return 0;
+}
