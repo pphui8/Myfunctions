0b409018971edf8d4d99d636204130d7aeb00574
pphui8 <55843852+pphui8@users.noreply.github.com>
2025-01-21 17:09:47 +0900

    update

---
+++ binary_search.cpp
@@ -0,0 +1,19 @@
+
+#define all(x) x.begin(), x.end()
+#define vec vector<lint>
+vec nums;
+
+int main() {
+    for(int i = 0; i < 1000; ++i) {
+        nums.push_back(i);
+    }
+
+    int target = 799;
+    int output = 0;
+
+    // binary search
+    output = upper_bound(all(nums), target) - nums.begin();
+
+    cout<<output<<endl;
+    return 0;
+}
